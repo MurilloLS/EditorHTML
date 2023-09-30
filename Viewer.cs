22e99b465de8ddf8fdb4b50d64a8d36ad5d0e707
Murillo Santos <murillo.lds01@gmail.com>
2023-09-29 23:48:44 -0300

    add EditorHtml

---
+++ Viewer.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace EditorHtml
+{
+    public class Viewer
+    {
+        public static void Show(string text)
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+            Console.WriteLine("MODO VISUALIZAÇÃO");
+            Console.WriteLine("-----------");
+            
+        }
+    }
+}
