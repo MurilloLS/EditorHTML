00fd4e1b4e4b1acf1118454b555ff914cffe803a
Murillo Santos <murillo.lds01@gmail.com>
2023-09-30 23:44:58 -0300

    modified Editor and Viewer

--- Viewer.cs
+++ Viewer.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Text.RegularExpressions;
 
 namespace EditorHtml
 {
@@ -11,8 +12,38 @@ namespace EditorHtml
             Console.ForegroundColor = ConsoleColor.Black;
             Console.Clear();
             Console.WriteLine("MODO VISUALIZAÇÃO");
-            Console.WriteLine("-----------");
-            
+            Console.WriteLine("-----------------");
+            Replace(text);
+            Console.WriteLine("-----------------");
+            Console.ReadKey();
+            Menu.Show();
+        }
+        public static void Replace(string text)
+        {
+            var strong = new Regex(@"<\s*strong[^>]*>(.*?)<\s*/\s*strong>");
+            var words = text.Split(' ');
+
+            for(var i = 0; i<words.Length; i++)
+            {
+                if(strong.IsMatch(words[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write(
+                        words[i].Substring(
+                            words[i].IndexOf('>') + 1,
+                            (
+                                (words[i].LastIndexOf('<') - 1) - 
+                                words[i].IndexOf('>')
+                            )
+                        )
+                    );
+                    Console.Write(" ");
+                }else{
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.Write(words[i]);
+                    Console.Write(" ");
+                }
+            }
         }
     }
 }
